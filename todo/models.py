4fd39ad65478cd98c494dda6b82f96d4d2c4ef18
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-06-24 22:17:48 +0300

    create todo model & add to admin panel

--- todo/models.py
+++ todo/models.py
@@ -3,5 +3,21 @@ from django.db import models
 # Create your models here.
 
 
+class Todo(models.Model):
 
+    PRIORITY=(
+        (1, 'High'),
+        (2, 'Medium'),
+        (3, 'Low'),
+    )
+  
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    priority = models.PositiveIntegerField(choices=PRIORITY, default=3)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
     
